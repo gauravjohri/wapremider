7e36b8e85dc2e062f6a07c6d939090fac22d86d0
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 04:35:45 +0530

    s

--- app/worker.py
+++ app/worker.py
@@ -2,8 +2,16 @@ from datetime import datetime
 import time
 from app.database import tasks
 from app.whatsapp import send_whatsapp
+from datetime import datetime, timezone
 
 print('Worker started...')
+print("NOW:", datetime.now())
+print("UTC NOW:", datetime.now(timezone.utc))
+
+
+
+
+
 while True:
     now = datetime.now()
     reminders = tasks.find({'reminder_time': {'$lte': now}, 'status': 'pending'})
