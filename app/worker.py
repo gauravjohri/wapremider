cd47da41952157bc66e93574e21401a618e3a733
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 04:38:26 +0530

    s

--- app/worker.py
+++ app/worker.py
@@ -3,13 +3,13 @@ import time
 from app.database import tasks
 from app.whatsapp import send_whatsapp
 from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 print('Worker started...')
 print("NOW:", datetime.now())
-print("UTC NOW:", datetime.now(timezone.utc))
-
-
-
+print("UTC NOW:", datetime.now(timezone.ist))
+now_ist = datetime.now(ZoneInfo("Asia/Kolkata"))
+print("IST NOW:", now_ist)
 
 
 while True:
