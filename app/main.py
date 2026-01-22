231a82dec863b1370da3c747324d2bcda4a975d5
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 01:03:29 +0530

    ss

--- app/main.py
+++ app/main.py
@@ -2,16 +2,35 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from datetime import datetime
 from app.database import tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="WhatsApp Reminder SaaS")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (DEV ONLY)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", response_class=HTMLResponse)
 def home():
     with open("frontend/index.html", "r", encoding="utf-8") as f:
         return f.read()
+    
+def normalize_phone(phone: str) -> str:
+    phone = phone.strip()              # remove spaces
+    phone = phone.replace(" ", "")     # remove internal spaces
+
+    if not phone.startswith("+"):
+        phone = "+" + phone
+
+    return phone
 
 @app.post("/create-reminder")
 def create_reminder(phone: str, message: str, time: str):
+    phone = normalize_phone(phone)
     tasks.insert_one({
         "user_phone": phone,
         "message": message,
--- app/whatsapp.py
+++ app/whatsapp.py
@@ -5,18 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 client = Client(os.getenv('TWILIO_SID'), os.getenv('TWILIO_AUTH'))
 
-def normalize_phone(phone: str) -> str:
-    phone = phone.strip()              # remove spaces
-    phone = phone.replace(" ", "")     # remove internal spaces
-
-    if not phone.startswith("+"):
-        phone = "+" + phone
-
-    return phone
-
-
 def send_whatsapp(phone, message):
-    phone = normalize_phone(phone)
     client.messages.create(
         from_=os.getenv('WHATSAPP_FROM'),
         body=message,
