2f772427c2e3f6a285fada1e2252c670336621f1
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 03:33:53 +0530

    s

--- test_twilio.py
+++ test_twilio.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 
-client = Client("ACea1c765b4a73e294779d651f1b5c4386", "90f00acaf0174ec51db9bc36e00983a4")
+client = Client("ACea1c765b4a73e294779d651f1b5c4386", "c725762bb41419a98ad761e718322946")
 
 client.messages.create(
     from_="whatsapp:+14155238886",
