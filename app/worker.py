25429e3bfbf7081e3ea4f533684f6ffbe1f75305
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 05:15:13 +0530

    s

--- app/worker.py
+++ app/worker.py
@@ -15,8 +15,9 @@ while True:
     now_utc = datetime.now(timezone.utc)
     now_ist = now_utc.astimezone(ZoneInfo("Asia/Kolkata"))
     print("NOW:", now_ist)
-    reminders = tasks.find({'reminder_time': {'$lte': now_ist}, 'status': 'pending'})
+    reminders = tasks.find({'status': 'pending'})
     for r in reminders:
-        send_whatsapp(r['user_phone'], r['message'])
-        tasks.update_one({'_id': r['_id']}, {'$set': {'status': 'done'}})
+        if r['reminder_time'].astimezone(IST) <= now_ist:
+            send_whatsapp(r['user_phone'], r['message'])
+            tasks.update_one({'_id': r['_id']}, {'$set': {'status': 'done'}})
     time.sleep(60)
