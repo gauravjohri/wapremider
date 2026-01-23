49208e82f5b173c982daccabe36f5c097058f9ed
gauravjohri <gaurav_johri@ymail.com>
2026-01-23 06:46:27 +0530

    ss

--- app/worker.py
+++ app/worker.py
@@ -6,14 +6,19 @@ from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
 print('Worker started...')
+print("NOW:", datetime.now())
+IST = ZoneInfo("Asia/Kolkata")
+
+
+
 while True:
     now_utc = datetime.now(timezone.utc)
     now_ist = now_utc.astimezone(ZoneInfo("Asia/Kolkata"))
+    print("NOW:", now_ist)
     reminders = tasks.find({'status': 'pending'})
     for r in reminders:
-        rt = r['reminder_time']
-        if datetime.fromisoformat(rt) <= now_ist:
+        if r['reminder_time'].astimezone(IST) <= now_ist:
             print("SENDING:", r['user_phone'])
             send_whatsapp(r['user_phone'], r['message'])
             tasks.update_one({'_id': r['_id']}, {'$set': {'status': 'done'}})
-    time.sleep(30)
+    time.sleep(1)
